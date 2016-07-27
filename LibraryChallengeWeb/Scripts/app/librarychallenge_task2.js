9a01e20e8edbe52588df64b3347a96b6caa7213d
Cronán Gogarty <cronangogarty@outlook.com>
2016-07-27 04:19:00 -0700

    Show all books & Show by category complete

--- LibraryChallengeWeb/Scripts/app/librarychallenge_task2.js
+++ LibraryChallengeWeb/Scripts/app/librarychallenge_task2.js
@@ -1,3 +1,47 @@
 ﻿$(document).ready(function () {
-    alert('library challenge task 2 ready');
+    //alert('library challenge task 2 ready');
+
+    document.getElementById('btnShowAll').addEventListener('click', showAll, false);
+    document.getElementById('selCategory').addEventListener('change', showByCategory, false);
+    var searchType = document.getElementById('searchType');
+
+    function showAll() {
+        var uri = 'api/library/books';
+        searchType.textContent = "Show All Books"
+        $('#library-challenge-results').empty();
+        $.getJSON(uri)
+            .done(function (data) {
+                $.each(data, function (key, item) {
+                    //$('<div>', { text: formatItem(item) }).appendTo('#library-challenge-results').addClass("bookDiv");
+                    $('#library-challenge-results').append('<div class="bookDiv"><h3>Title:&nbsp;' + item.title + '</h3><div><strong>Author:</strong>&nbsp;' + item.author + '<div><div><strong>ISBN:</strong>&nbsp;' + item.isbn + '</div><div><strong>Category:</strong>&nbsp;' + item.category + '</div><div><strong>Published Date:</strong>&nbsp;' + item.publishedDate + '</div><div><strong>On Loan To:</strong>&nbsp;' + item.lentToCustomerId + '</div><div><strong>Due Date:</strong>&nbsp;' + item.dueDate + '</div></div>');
+                });
+            });
+    }
+
+    function showByCategory() {
+        
+        var e = document.getElementById('selCategory');
+        if (e.selectedIndex != 0) {
+            
+            var selectedCategory = e.options[e.selectedIndex].text;
+            var uri = 'api/library/books/' + selectedCategory;
+            searchType.textContent = "Show All " + selectedCategory + " Books";
+            $('#library-challenge-results').empty();
+
+            $.getJSON(uri)
+                .done(function (data) {
+                    $.each(data, function (key, item) {
+                        //$('<div>', { text: formatItem(item) }).appendTo('#library-challenge-results').addClass("bookDiv");
+                        $('#library-challenge-results').append('<div class="bookDiv"><h3>Title:&nbsp;' + item.title + '</h3><div><strong>Author:</strong>&nbsp;' + item.author + '<div><div><strong>ISBN:</strong>&nbsp;' + item.isbn + '</div><div><strong>Category:</strong>&nbsp;' + item.category + '</div><div><strong>Published Date:</strong>&nbsp;' + item.publishedDate + '</div><div><strong>On Loan To:</strong>&nbsp;' + item.lentToCustomerId + '</div><div><strong>Due Date:</strong>&nbsp;' + item.dueDate + '</div></div>');
+                    })
+                });
+        }
+        
+    }
+    
+
+    function formatItem(item) {
+        //return JSON.stringify(item);
+    }
+
 });
